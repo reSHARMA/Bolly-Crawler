c94cbd48c2f6a554f7689a6cbb23b072941370a3
reshabh <Reshabh Sharma>
2017-07-30 23:19:17 +0530

    updated for python 3.0

--- movie.py
+++ movie.py
@@ -1,17 +1,20 @@
 from bs4 import BeautifulSoup
-import urllib
+import urllib.request
+import urllib.parse
 f=open('movie.txt','w')
+s = ''
 for i in range(65,65+26):
-	i = str(unichr(i))
-	r =  urllib.urlopen('http://www.bollywoodhungama.com/directory/movies-list/alphabet/'+i).read()
+	i = str(chr(i))
+	r =  urllib.request.urlopen('http://www.bollywoodhungama.com/directory/movies-list/alphabet/'+i).read()
 	soup = BeautifulSoup(r,'html.parser')
 	page = int(soup.select('.dots + a')[0].string)
 	for x in range(1,page):
-		rr =  urllib.urlopen('http://www.bollywoodhungama.com/directory/movies-list/alphabet/'+i+'/page/'+str(x)).read()
+		rr =  urllib.request.urlopen('http://www.bollywoodhungama.com/directory/movies-list/alphabet/'+i+'/page/'+str(x)).read()
 		soupp = BeautifulSoup(rr,'html.parser')
 		souppp = soupp.select('.directory--movie li a')
 		for b in souppp:
-			s = '\n'.join(b.attrs['title'].encode(encoding='UTF-8',errors='ignore'))
+			s =  str(b.attrs['title'].encode(encoding='UTF-8',errors='ignore')) + '\n'
 #			s = `'\n'.join(b.attrs[3])
-f.write(s)
+			f.write(s)
+#f.write(s)
 f.close()
